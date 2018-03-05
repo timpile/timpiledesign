f2bad1c17e719bb38f22f6121a72bc6cea81bdf3
Tim Pile <tim@gritchat.com>
2018-03-05 06:43:56 -0500

    Generated message model and tests to for contact form

---
+++ test/models/message_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class MessageTest < ActiveSupport::TestCase
+
+  test 'should be valid when all attributes are set' do
+    attrs = {
+      name: 'stephen',
+      email: 'stephen@example.org',
+      body: 'kthnxbai'
+    }
+
+    msg = Message.new attrs
+    assert msg.valid?, 'should be valid'
+  end
+
+  test 'name, email and body are required by law' do
+    msg = Message.new
+
+    refute msg.valid?, 'Blank Mesage should be invalid'
+
+    assert_match /blank/, msg.errors[:name].to_s
+    assert_match /blank/, msg.errors[:email].to_s
+    assert_match /blank/, msg.errors[:body].to_s
+  end
+end
