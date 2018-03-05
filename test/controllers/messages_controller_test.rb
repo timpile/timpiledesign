a44117ba30da03d5fa01fca840eb561fde468a29
Tim Pile <tim@gritchat.com>
2018-03-05 06:55:02 -0500

    Updated message controller and tests to handle create action for contact form

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -15,4 +15,29 @@ class MessagesControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "POST create" do
+    post create_message_url, params: {
+      message: {
+        name: 'guest',
+        email: 'guest@example.org',
+        body: 'Hello'
+      }
+    }
+
+    assert_redirected_to new_message_url
+
+    follow_redirect!
+
+    assert_match /Message received, thanks!/, response.body
+  end
+
+  test "invalid POST create" do
+    post create_message_url, params: {
+      message: { name: '', email: '', body: '' }
+    }
+
+    assert_match /Name .* blank/, response.body
+    assert_match /Email .* blank/, response.body
+    assert_match /Body .* blank/, response.body
+  end
 end
